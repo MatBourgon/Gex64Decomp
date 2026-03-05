c2c6ea8b049a6c720c8aeeb3a65febf0ed251e70
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-05 05:06:52 +0000

    moar

---
+++ src/io/leointerrupt.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/io/leointerrupt", __osLeoInterrupt);
+
+INCLUDE_ASM("asm/nonmatchings/io/leointerrupt", __osLeoAbnormalResume);
+
+INCLUDE_ASM("asm/nonmatchings/io/leointerrupt", __osLeoResume);
