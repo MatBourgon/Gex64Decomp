4d617a17136498f0b374083b3c6b1c63dd4fe856
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-06 04:48:02 +0000

    re-added logo code

--- src/logo_code.c
+++ src/logo_code.c
@@ -1,30 +1,282 @@
 #include "common.h"
+#include "types/GameState.h"
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_select_OnCreate);
+#include <PR/gbi.h>
+#include <PR/mbi.h>
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_select_OnUpdate);
+#include "types/Level.h"
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_cryslogo_OnCreate);
+extern Gfx D_8006D578[];
+extern char D_8014F34C;
+extern Gfx* D_80157050;
 
-INCLUDE_RODATA("asm/nonmatchings/logo_code", D_8015A0A4_ECEB4);
+extern int D_8006FA54;
+extern char D_80078171;
+extern int D_800B83C8;
+extern int D_800E5CD4;
+extern short D_800E5DB0;
+extern int D_800E97CC;
+extern char D_8014F354;
+extern int D_80153A58;
+extern int D_80156BDC;
+extern int D_80157034;
+extern int D_801574FC;
+extern char D_8006CF20;
+extern short* D_8006CFA0;
+extern unsigned short D_800E5DB2;
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_cryslogo_OnUpdate);
+void logo_select_OnCreate(Level_t* level, int** arg1)
+{
+    level->flags |= 0xC00;
+    arg1[0xC/4][0x10/4] |= 0x800;
+}
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_mwgex_OnCreate);
+void logo_select_OnUpdate()
+{
+    func_8002C1AC(0);
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_mwgex_OnUpdate);
+    switch (D_8006FA54) {
+        
+    case 1:
+        D_8006FA54 = 2;
+        osSendMesg(&D_80153A58, 1, 0);
+    break;
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_mwlogo_OnCreate);
+    case 3:
+        if (D_80156BDC != 0)
+        {
+            func_80040170(10);
+            D_8006FA54 = 12;
+        }
+        else
+        {
+            D_80078171 = 1;
+            D_8006FA54 = 8;
+            osSendMesg(&D_80153A58, 3, 0);
+        }
+        break;
+
+    case 9:
+        D_8006FA54 = 4;
+        osSendMesg(&D_80153A58, 2, 0);
+        break;
+
+    case 5:
+        if (D_80156BDC != 0)
+        {
+            func_80040170(10);
+            D_8006FA54 = 12;
+        }
+        else if (D_800E5DB0 & 0x1000)
+        {
+            if (D_800E97CC != 0)
+            {
+                D_800E5CD4 = 0;
+                D_8006FA54 = 6;
+            }
+            else
+            {
+                func_80040170(12);
+                D_8006FA54 = 12;
+            }
+        }
+        else if ((D_800B83C8 < 0x100) || (D_800E97CC >= 0x10))
+        {
+            D_8014F354 = 0;
+            func_80040170(20);
+        }
+        else
+        {
+            D_8006FA54 = 13;
+        }
+
+        break;
+
+    case 6:
+        if ((D_80156BDC != 0) && (D_80156BDC != 5))
+        {
+            func_80040170(10);
+            D_8006FA54 = 12;
+        }
+        else if (D_800E5CD4 < D_80157034)
+        {
+            D_8006FA54 = 7;
+            osSendMesg(&D_80153A58, 4, 0);
+        }
+        else
+        {
+            D_8014F354 = 0;
+            func_80040170(11);
+            D_8014F34C = func_80014DD4(-1);
+            D_801574FC = D_8014F34C;
+            D_8006FA54 = 12;
+        }
+        break;
+
+    case 13:
+        func_80040170(18);
+        break;
+    }
+}
+
+void logo_cryslogo_OnCreate(Level_t* level, short** arg1)
+{
+    D_8006CF20 = 0;
+    D_8006FA54 = 0xD;
+    level->_104 = 0;
+    level->flags |= 0x400;
+    func_8002C1AC(0);
+    arg1[0x8/4][0x30/2] = 0xFA0;
+    level->_60[0] = 0x3FE;
+    level->_60[1] = 0x6E1;
+    level->_60[2] = 1;
+    level->_40[4] = 0x10EC;
+    level->_40[5] = 0x12C0;
+    level->_40[6] = -0x460;
+}
+
+void logo_cryslogo_OnUpdate(Level_t* level, int** arg1)
+{
+    func_8002C1AC(0);
+    arg1[0xC/4][0x10/4] |= 0x800;
+    
+    if (level->_104 < 0x50)
+    {
+        level->_104++;
+    }
+
+    if ((D_800E5DB2 & 0x9000) || (level->_104 == 0x50))
+    {
+        func_800396E0("logo", "logo1", gpGameState8);
+    }
+}
+
+void logo_mwgex_OnCreate(Level_t* level)
+{
+    level->_60[2] = 0xC8;
+    level->_104 = 0x50;
+    level->flags |= 0x400;
+}
+
+FORCE_INLINE void logo_mwgex_OnUpdate_dec(int* a)
+{
+    (*a)--;
+}
+
+void logo_mwgex_OnUpdate(Level_t* level)
+{
+    logo_mwgex_OnUpdate_dec(&level->_104);
+    
+    if ((D_800E5DB2 & 0x9000) != 0 || level->_104 == 0)
+    {
+        func_800396E0("logo", "logo2", gpGameState8);
+    }
+    
+}
+
+void logo_mwlogo_OnCreate(Level_t* level, short** arg1)
+{
+    level->_60[0] = 0x802;
+    level->_60[1] = -0x400;
+    level->_60[2] = 0;
+    level->_40[4] = arg1[0x8/4][0] + 0x32;
+    level->_40[5] = 0x64;
+    level->_40[6] = 0x64;
+}
 
 void logo_mwlogo_OnUpdate(void) {
 }
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_rtlogor_OnCreate);
+void logo_rtlogor_OnCreate(Level_t* level, short** arg1)
+{
+    level->_104 = 0;
+    level->flags |= 0x400;
+    func_8002C1AC(0);
+    level->_60[0] = 0x400;
+    level->_60[1] = 0x6D6;
+    level->_40[4] = 0x1306;
+    level->_40[5] = 0x1194;
+    level->_60[2] = 0;
+    level->_40[6] = -0x96;
+    arg1[0x8/4][0x4/2] += 0xE6;
+    arg1[0x8/4][0x2/2] -= 0xC8;
+    arg1[0x8/4][0] = D_8006CFA0[0x48/2];
+    level->_4E = 0;
+    level->_50[7] = 0;
+}
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", logo_rtlogor_OnUpdate);
+void logo_rtlogor_OnUpdate(Level_t* level)
+{
+    if (level->_104 < 0x50) {
+        level->_104++;
+    }
+    
+    if ((D_800E5DB2 & 0x9000) || (level->_104 == 0x50)) {
+        func_8001A790();
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", func_80159CC8_ECAD8);
+void func_80159CC8_ECAD8()
+{
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", func_80159DA0_ECBB0);
+    gSPDisplayList(D_80157050++, D_8006D578);
 
-INCLUDE_ASM("asm/nonmatchings/logo_code", func_80159EF0_ECD00);
+    func_80030DD8("THERE IS NO CONTROLLER", 0x44, 0x46, 1);
+    func_80030DD8("CONNECTED IN CONTROLLER", 0x3E, 0x64, 1);
+    func_80030DD8("SOCKET ONE. SHUT OFF", 0x48, 0x82, 1);
+    func_80030DD8("POWER, INSERT A", 0x55, 0xA0, 1);
+    func_80030DD8("CONTROLLER AND RESTART", 0x3D, 0xBE, 1);
+
+    gDPPipeSync(D_80157050++);
+}
+
+void func_80159DA0_ECBB0()
+{
+
+    gSPDisplayList(D_80157050++, D_8006D578);
+
+    func_80030DD8("GEX]: ENTER THE GECKO", 0x46, 0x32, 1);
+    func_80030DD8("[1998 CRYSTAL DYNAMICS.", 0x3C, 0x46, 1);
+    func_80030DD8("CRYSTAL DYNAMICS, THE CRYSTAL", 0x26, 0x64, 1);
+    func_80030DD8("DYNAMICS LOGO, GEX AND THE", 0x32, 0x78, 1);
+    func_80030DD8("GEX CHARACTER ARE TRADEMARKS", 0x21, 0x8C, 1);
+    func_80030DD8("OF CRYSTAL DYNAMICS.", 0x46, 0xA0, 1);
+    func_80030DD8("ALL RIGHTS RESERVED.", 0x4B, 0xB4, 1);
+    func_80030DD8("LICENSED BY NINTENDO", 0x50, 0xD2, 1);
+
+    gDPPipeSync(D_80157050++);
+    
+    if (D_8014F34C > 60) {
+        func_80040170(0x1C);
+        return;
+    }
+
+    D_8014F34C++;
+}
+
+void func_80159EF0_ECD00(int arg0)
+{
+
+    gSPDisplayList(D_80157050++, D_8006D578);
+
+    func_80030DD8("PUBLISHED BY", 0x6E, 0x28, 1);
+    func_80030DD8("MIDWAY HOME ENTERTAINMENT INC.", 0x21, 0x3C, 1);
+    func_80030DD8("MIDWAY IS A TRADEMARK", 0x46, 0x5A, 1);
+    func_80030DD8("OF MIDWAY GAMES INC.", 0x4B, 0x6E, 1);
+    func_80030DD8("DISTRIBUTED BY MIDWAY HOME", 0x37, 0x82, 1);
+    func_80030DD8("ENTERTAINMENT INC.", 0x55, 0x96, 1);
+    func_80030DD8("UNDER LICENSE", 0x66, 0xAA, 1);
+    func_80030DD8("N64 DEVELOPMENT BY", 0x50, 0xC8, 1);
+    func_80030DD8("REALTIME ASSOCIATES INC.", 0x41, 0xDC, 1);
+
+    gDPPipeSync(D_80157050++);
+
+    if (D_8014F34C > 60)
+    {
+        func_800396E0(0, "logo4", arg0);
+        return;
+    }
+
+    D_8014F34C += 1;
+}
+
