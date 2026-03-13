a0e31c25dba9c5bc0fb09f142ae70f2345f0a280
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-13 04:07:16 +0000

    MOOSHU

--- src/level/MOOSHU.c
+++ src/level/MOOSHU.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-#include "types/Instance.h"
+#include "level/MOOSHU.h"
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_80159720_C20A0);
 
@@ -14,7 +14,7 @@ INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_80159D0C_C268C);
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_80159EC4_C2844);
 
-void func_8015A07C_C29FC(void) {
+void func_8015A07C_C29FC(Instance* instance, GameTracker* gameTracker) {
 }
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015A084_C2A04);
@@ -84,7 +84,7 @@ INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015BDB4_C4734);
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015C2E0_C4C60);
 
-void mooshu_moolevr_OnCreate(Instance* instance)
+void mooshu_moolevr_OnCreate(Instance* instance, GameTracker* gameTracker)
 {
     instance->flags |= 0x400;
 }
@@ -96,7 +96,7 @@ INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", mooshu_moolevr_OnCollide);
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015C6D0_C5050);
 
-void func_8015C708_C5088(Instance* instance)
+void func_8015C708_C5088(Instance* instance, GameTracker* gameTracker)
 {
     instance->_F4[2] = 0;
 }
@@ -105,7 +105,7 @@ INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015C710_C5090);
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015C780_C5100);
 
-void mooshu_moosprk_OnCreate(Instance* instance)
+void mooshu_moosprk_OnCreate(Instance* instance, GameTracker* gameTracker)
 {
     instance->_E0[1] = -0x10;
 }
@@ -128,7 +128,7 @@ INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015D544_C5EC4);
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", func_8015D6B0_C6030);
 
-void func_8015D7B4_C6134(Instance* instance)
+void func_8015D7B4_C6134(Instance* instance, GameTracker* gameTracker)
 {
     instance->_F0[6]++;
 }
@@ -141,7 +141,7 @@ INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", mooshu_ebolt_OnCreate);
 
 INCLUDE_ASM("asm/nonmatchings/level/MOOSHU", mooshu_ebolt_OnUpdate);
 
-void mooshu_vandb_OnCreate(Instance* instance)
+void mooshu_vandb_OnCreate(Instance* instance, GameTracker* gameTracker)
 {
     instance->flags |= 0x400;
 }
