96c6ba76789ca26db8e0c77cbb0731a5f1d1928c
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-13 14:57:28 +0000

    Split out GENERIC.c, processed GenericInit and GenericProcess

---
+++ src/GENERIC.c
@@ -0,0 +1,37 @@
+#include "common.h"
+
+#include "types/Instance.h"
+#include "types/GameTracker.h"
+
+void GenericInit(Instance* instance, GameTracker* gameTracker) {
+    void* spline; // Spline*
+
+    spline = NULL;
+    
+    if (instance != NULL) {
+        if (instance->_1C != NULL) { // instance->intro
+            if (instance->_1C[0x28/4] != NULL) { // instance->intro->multiSpline
+                spline = ((void***)instance->_1C)[0x28/4][0]; // instance->intro->multiSpline->position
+            }
+        }
+    }
+    
+    if (spline == NULL) {
+        instance->_E0[1] = -10; // zAccl
+        instance->_E0[2] = 100; // maxXVel
+        instance->_E0[3] = 100; // maxYVel
+        instance->_F4[-1] = 100; // maxZVel
+    }
+    
+}
+
+void GenericProcess(Instance* instance, GameTracker* gameTracker)
+{
+    if (instance->_18[4] != 0)
+    {
+        func_8002DAF8(instance, -1);
+    }
+    func_8001DA8C(instance, gameTracker);
+}
+
+INCLUDE_ASM("asm/nonmatchings/GENERIC", GenericCollide);
