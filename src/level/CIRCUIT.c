5739a320ea643ffc5af2dd4d4d639deadead23ed
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-13 03:55:12 +0000

    CIRCUIT

--- src/level/CIRCUIT.c
+++ src/level/CIRCUIT.c
@@ -1,8 +1,6 @@
 #include "common.h"
 
-#include "types/Instance.h"
-
-#include "types/GameTracker.h"
+#include "level/CIRCUIT.h"
 
 #include "types/Vector.h"
 extern int* PlayerInstance;
@@ -27,13 +25,13 @@ INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_bouncer_OnUpdate);
 
 INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_bouncer_OnCollide);
 
-void circuit_crawler_OnCreate(Instance* instance)
+void circuit_crawler_OnCreate(Instance* instance, GameTracker* gameTracker)
 {
     instance->_F4[0] = 0;
     instance->_4E = 0;
 }
 
-void circuit_crawler_OnUpdate(Instance* instance) {
+void circuit_crawler_OnUpdate(Instance* instance, GameTracker* gameTracker) {
     int a0, v0, v1;
 
     instance->flags2 &= ~0x10;
@@ -57,7 +55,7 @@ void circuit_crawler_OnUpdate(Instance* instance) {
     }
 }
 
-void circuit_crawler_OnCollide(Instance* instance, int* arg1) {
+void circuit_crawler_OnCollide(Instance* instance, GameTracker* gameTracker) {
     int temp_a3;
     int temp_v1;
     char** temp_a2;
@@ -65,7 +63,7 @@ void circuit_crawler_OnCollide(Instance* instance, int* arg1) {
     temp_a2 = ((char***)instance->_70)[2];
     temp_a3 = ((short*)temp_a2)[3];
     if (temp_a3 == 1) {
-        if ((temp_a2[5] == (char*)arg1[12/4]) && (temp_a2[12/4][5] >= 6U)) {
+        if ((temp_a2[5] == ((char**)gameTracker)[12/4]) && (temp_a2[12/4][5] >= 6U)) {
             if (instance->_F4[0] == 0)
             {
                 ((char*)instance->_40)[0xe] = 1;
@@ -78,7 +76,7 @@ void circuit_crawler_OnCollide(Instance* instance, int* arg1) {
             {
                 func_80047904(instance, 5, 3, 0);
             }
-        } else if ((((short*)temp_a2)[3] == 1) && (temp_a2[5] == (char*)arg1[12/4]) && ((instance->_F4[0] - 1) >= 2U)) {
+        } else if ((((short*)temp_a2)[3] == 1) && (temp_a2[5] == ((char**)gameTracker)[12/4]) && ((instance->_F4[0] - 1) >= 2U)) {
             func_80022714(instance);
         }
     }
@@ -154,7 +152,7 @@ INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_follow_OnCollide);
 
 INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", func_8015D780_84960);
 
-void circuit_pball_OnCreate(Instance* instance)
+void circuit_pball_OnCreate(Instance* instance, GameTracker* gameTracker)
 {
     instance->flags |= 0x100000;
 }
@@ -183,10 +181,10 @@ INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", func_8015E140_85320);
 
 INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_fxgen_OnUpdate);
 
-void circuit_fxgen_OnCollide(void) {
+void circuit_fxgen_OnCollide(Instance* instance, GameTracker* gameTracker) {
 }
 
-void circuit_qmark_OnCreate(Instance* instance)
+void circuit_qmark_OnCreate(Instance* instance, GameTracker* gameTracker)
 {
     instance->_104 = 0;
     instance->_F4[2] = 0x40;
@@ -232,7 +230,7 @@ void circuit_qmark_OnUpdate(Instance* instance, GameTracker* arg1) {
     instance->_60[2] = ((instance->_60[2] + temp_s0[0]) & 0xFFF);
 }
 
-void circuit_qmark_OnCollide(Instance* instance) {
+void circuit_qmark_OnCollide(Instance* instance, GameTracker* gameTracker) {
     short* temp_s1;
 
     temp_s1 = (short*)instance->_20[1];
@@ -308,7 +306,7 @@ INCLUDE_RODATA("asm/nonmatchings/level/CIRCUIT", D_8016361C_8A7FC);
 
 INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_bomb_OnUpdate);
 
-void circuit_bomb_OnCollide(void) {
+void circuit_bomb_OnCollide(Instance* instance, GameTracker* gameTracker) {
 }
 
 INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_explode_OnCreate);
@@ -343,7 +341,7 @@ INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_babyres_OnUpdate);
 
 INCLUDE_ASM("asm/nonmatchings/level/CIRCUIT", circuit_babyres_OnCollide);
 
-void circuit_btimer_OnCreate(Instance* instance, int* arg1) {
+void circuit_btimer_OnCreate(Instance* instance, GameTracker* gameTracker) {
     int var_s0;
     short* temp_a2;
     int* temp_v1;
@@ -354,9 +352,9 @@ void circuit_btimer_OnCreate(Instance* instance, int* arg1) {
     instance->_F0[6] = (unsigned short)temp_a2[1];
     *(short*)&instance->_100 = 0;
     instance->flags |= 0xC00;
-    temp_v1 = (int*)arg1[3];
+    temp_v1 = ((int**)gameTracker)[3];
     temp_v1[0xFC/4] |= 0x4000;
-    temp_v1_2 = (int*)arg1[3];
+    temp_v1_2 = ((int**)gameTracker)[3];
     temp_v1_2[4] |= 0x100;
     func_8002CA2C(4, temp_a2[1], temp_a2);
     for (var_s0 = 1; var_s0 < 4; var_s0++) {
@@ -365,7 +363,7 @@ void circuit_btimer_OnCreate(Instance* instance, int* arg1) {
     instance->_F4[1] = 0;
 }
 
-void circuit_btimer_OnUpdate(Instance* instance, int** arg1) {
+void circuit_btimer_OnUpdate(Instance* instance, GameTracker* gameTracker) {
     char sp10[0x50];
     int temp_s0;
     int temp_s3_2;
@@ -379,16 +377,16 @@ void circuit_btimer_OnUpdate(Instance* instance, int** arg1) {
     temp_s2 = &instance->_F4[2];
     if (*(short*)&instance->_100 == 0) {
         if (((short*)temp_s2)[0] != 0) {
-            if ((int)arg1[0x4BFC/4] < arg1[0x4/4][0x34/4]) {
+            if ((int)(((int**)gameTracker))[0x4BFC/4] < ((int**)gameTracker)[0x4/4][0x34/4]) {
                 if (D_80154834 != 0) {
                     *(short*)&instance->_108 = 1;
                 }
                 func_80037B00(0x64, 0x69);
                 Print3DTextf("#2COLLECT");
-                sprintf(sp10, "%2d", arg1[0x4/4][0x34/4]);
+                sprintf(sp10, "%2d", ((int**)gameTracker)[0x4/4][0x34/4]);
                 func_80037B00(0x8C, 0x91);
                 Print3DTextf(sp10);
-                arg1[0xC/4][0x10/4] |= 0x100;
+                ((int**)gameTracker)[0xC/4][0x10/4] |= 0x100;
             } else {
                 func_8002C18C(4);
                 func_80037B00(0x64, 0x64);
@@ -405,10 +403,10 @@ void circuit_btimer_OnUpdate(Instance* instance, int** arg1) {
                 ((short*)temp_s2)[0xC/2] = 0;
             }
         }
-        if ((((short*)arg1)[0x4C12/2] == 0) && (var_v1 != 0) && (instance->_1C[0x2C/4] == 0)) {
+        if ((((short*)((int**)gameTracker))[0x4C12/2] == 0) && (var_v1 != 0) && (instance->_1C[0x2C/4] == 0)) {
             temp_s2[0x8/4] -= D_800E5FD8;
         }
-        if (((arg1[0xC/4][0xFC/4] & 0x600000) == 0x600000) && (instance->_F4[1] == 0)) {
+        if (((((int**)gameTracker)[0xC/4][0xFC/4] & 0x600000) == 0x600000) && (instance->_F4[1] == 0)) {
             ((short*)temp_s2)[0] = (((unsigned short*)temp_s3)[1] - 1);
             if (temp_s3[0x4/4] == 0x3F2) {
                 SIGNAL_HandleSignal(PlayerInstance, temp_s3[0x8/4] + 4, 0);
@@ -416,18 +414,18 @@ void circuit_btimer_OnUpdate(Instance* instance, int** arg1) {
             instance->_F4[1] = 1;
             PlayerInstance[0xFC/4] &= 0xFFBFFFFF;
         }
-        if ((arg1[0xC/4][0xFC/4] & 0x400000) && ((arg1[0x4C00/4] != 0) || (arg1[0x4C04/4] != 0))) {
+        if ((((int**)gameTracker)[0xC/4][0xFC/4] & 0x400000) && ((((int**)gameTracker)[0x4C00/4] != 0) || (((int**)gameTracker)[0x4C04/4] != 0))) {
             func_8002C18C(5);
             temp_s2[0x8/4] = 0x3C;
             ((short*)temp_s2)[2] = 1;
-            arg1[0xC/4][0x10/4] |= 0x100;
+            ((int**)gameTracker)[0xC/4][0x10/4] |= 0x100;
         }
-        if ((temp_s2[0x8/4] < 0) && (arg1[0x4C00/4] == 0) && (arg1[0x4C04/4] == 0)) {
-            arg1[0x4BFC/4] = 0;
+        if ((temp_s2[0x8/4] < 0) && (((int**)gameTracker)[0x4C00/4] == 0) && (((int**)gameTracker)[0x4C04/4] == 0)) {
+            ((int**)gameTracker)[0x4BFC/4] = 0;
             func_8002C18C(5);
             temp_s2[0x8/4] = 0x3C;
             ((short*)temp_s2)[2] = 2;
-            arg1[0xC/4][0x10/4] |= 0x100;
+            ((int**)gameTracker)[0xC/4][0x10/4] |= 0x100;
         }
         if (((short*)temp_s2)[2] == 0) {
             temp_s0 = temp_s2[0x8/4];
@@ -445,17 +443,17 @@ void circuit_btimer_OnUpdate(Instance* instance, int** arg1) {
                 sprintf(sp10, "%2d", temp_s6 + 0x64);
                 Print3DTextf(&sp10[1]);
             }
-            if ((((short*)temp_s2)[0] == 0) && ((int)arg1[0x4BFC/4] < arg1[0x4/4][0x34/4])) {
+            if ((((short*)temp_s2)[0] == 0) && ((int)((int**)gameTracker)[0x4BFC/4] < ((int**)gameTracker)[0x4/4][0x34/4])) {
                 func_80037B00(0xF0, 0xC8);
-                sprintf(sp10, "%2d", arg1[0x4/4][0x34/4] - (int)arg1[0x4BFC/4]);
+                sprintf(sp10, "%2d", ((int**)gameTracker)[0x4/4][0x34/4] - (int)((int**)gameTracker)[0x4BFC/4]);
                 Print3DTextf(sp10);
             }
         }
     } else {
-        arg1[0xC/4][0x10/4] |= 0x100;
+        ((int**)gameTracker)[0xC/4][0x10/4] |= 0x100;
         if (*(short*)&instance->_100 == 2) {
             if (--instance->_104 < 0) {
-                func_800396E0("map", "map5", arg1);
+                func_800396E0("map", "map5", ((int**)gameTracker));
                 return;
             }
             func_80037B00(0x64, 0x64);
