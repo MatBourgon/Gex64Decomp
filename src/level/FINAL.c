a43e944fd0f7c996e178efc50931ad77e32eea7d
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-14 02:22:54 +0000

    Changed pointer type as we don't know what it actually is

--- src/level/FINAL.c
+++ src/level/FINAL.c
@@ -2,7 +2,6 @@
 
 #include "level/FINAL.h"
 #include "types/G2String.h"
-#include "types/obtable.h"
 
 INCLUDE_ASM("asm/nonmatchings/level/FINAL", final_oldpoptv_OnCreate);
 
@@ -209,7 +208,7 @@ void final_frez_OnCreate(Instance* instance, GameTracker* gameTracker) {
         instance->_C0[6] = 1;
         instance->_40[6] -= 0x12C;
         instance->_E0[2] = OBTABLE_FindObject(D_8016166C_9280C);
-        ((objFunction*)instance->_B0)[1] = &func_8015F200_903A0;
+        ((void**)instance->_B0)[1] = (void*)&func_8015F200_903A0;
         instance->flags |= 0x10400;
         instance->flags2 |= 8;
     }
