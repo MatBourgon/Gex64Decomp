9f987173b0b76017a836a2fe068b4def582282b0
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-16 21:57:48 +0000

    level button stuff

--- src/_52ee0.c
+++ src/_52ee0.c
@@ -30,6 +30,46 @@ INCLUDE_ASM("asm/nonmatchings/_52ee0", func_80052AA8);
 INCLUDE_ASM("asm/nonmatchings/_52ee0", func_80052ADC);
 
 INCLUDE_ASM("asm/nonmatchings/_52ee0", func_80052C2C);
+/*void func_80052C2C(s32 arg0) {
+    short var_a0;
+    int temp_a0;
+
+    if (D_80154834 != 0) return;
+        temp_a0 = arg0 - 0x45;
+        
+    switch (arg0) {
+        case 0x49:
+            var_a0 = 0x1D9;
+            break;
+        case 0x47:
+            var_a0 = 0x1DB;
+            break;
+        case 0x4D:
+            rand(temp_a0);
+            var_a0 = 0x1DA;
+            break;
+        case 0x45:
+            var_a0 = 0x1DA;
+            break;
+        case 0x4B:
+            var_a0 = 0x1DC;
+            break;
+        case 0x4A:
+            var_a0 = 0x1DD;
+            break;
+        case 0x46:
+            var_a0 = 0x1DE;
+            break;
+        case 0x48:
+            var_a0 = (D_80079460[((short)(rand(temp_a0) % 3))]);
+            break;
+        default:
+            return;
+    }
+    D_80154834 = 3;
+    D_80154833 = 0;
+    D_800E5CE0 = func_80050B64(var_a0);
+}*/
 
 INCLUDE_ASM("asm/nonmatchings/_52ee0", func_80052D18);
 
