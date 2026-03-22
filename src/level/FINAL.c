89e9f94dc69f9b18c1b3803cbb28896c02bc8e17
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-22 20:36:57 +0000

    update frezc

--- src/level/FINAL.c
+++ src/level/FINAL.c
@@ -275,7 +275,7 @@ void final_frez_OnCollide(Instance* instance, GameTracker* gameTracker) {
     temp_s1 = &instance->_C0[2];
     if (G2String_Compare_EQ(bsp->instanceSpline->object->name, D_8016166C_9280C)) {
         if (instance->_F4[0] != 0) {
-            func_800331BC(instance->_F4[0], gameTracker);
+            func_800331BC(instance->_F4[0]);
             instance->_F4[0] = 0;
         }
         if (*temp_s1 != 0xB) {
