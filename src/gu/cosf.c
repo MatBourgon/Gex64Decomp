3501e7c2354352f7af1494c92410ea77a0ee63f2
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-24 14:31:26 +0000

    perspective

--- src/gu/cosf.c
+++ src/gu/cosf.c
@@ -1,5 +1,8 @@
 #include "common.h"
 
+#include <gu/guint.h>
+extern const du D_8007EC60[];
+
 INCLUDE_RODATA("asm/nonmatchings/gu/cosf", __libm_qnan_f);
 
 INCLUDE_ASM("asm/nonmatchings/gu/cosf", cosf);
--- src/gu/perspective.c
+++ src/gu/perspective.c
@@ -1,26 +1,58 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/gu/perspective", guPerspectiveF);
+#include <gu/guint.h>
+
+void guPerspectiveF(float mf[4][4], u16 *perspNorm, float fovy, float aspect, float near, float far, float scale)
+{
+	float	cot;
+	int	i, j;
+
+	guMtxIdentF(mf);
+
+	fovy *= 3.1415926 / 180.0;
+	cot = cosf (fovy/2) / sinf (fovy/2);
+
+	mf[0][0] = cot / aspect;
+	mf[1][1] = cot;
+	mf[2][2] = (near + far) / (near - far);
+	mf[2][3] = -1;
+	mf[3][2] = (2 * near * far) / (near - far);
+	mf[3][3] = 0;
+
+	for (i=0; i<4; i++)
+	    for (j=0; j<4; j++)
+		mf[i][j] *= scale;
+
+	if (perspNorm != (u16 *) NULL) {
+	    if (near+far<=2.0) {
+		*perspNorm = (u16) 0xFFFF;
+	    } else  {
+		*perspNorm = (u16) ((2.0*65536.0)/(near+far));
+		if (*perspNorm<=0) 
+		    *perspNorm = (u16) 0x0001;
+	    }
+	}
+}
 
 INCLUDE_ASM("asm/nonmatchings/gu/perspective", guPerspective);
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC58);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC58); // (...), P[0]
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC68);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC68); // P[1]
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC70);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC70); // P[2]
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC78);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC78); // P[3]
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC80);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC80); // P[4]
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC88);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC88); // rpi
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC90);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC90); // pihi
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC98);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007EC98); // pilo
 
-INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007ECA0);
+INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007ECA0); // zero
 
 INCLUDE_RODATA("asm/nonmatchings/gu/perspective", D_8007ECA8);
 
