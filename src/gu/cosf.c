ea9708a235bef70000b1102bff817ded32757d82
Mathieu Bourgon <matbourgon@gmail.com>
2026-03-24 14:00:05 +0000

    __libm_qnan_f

--- src/gu/cosf.c
+++ src/gu/cosf.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-INCLUDE_RODATA("asm/nonmatchings/gu/cosf", D_8007ED30);
+INCLUDE_RODATA("asm/nonmatchings/gu/cosf", __libm_qnan_f);
 
 INCLUDE_ASM("asm/nonmatchings/gu/cosf", cosf);
 
