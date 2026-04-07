7f9f79af8949f1970119d4268fa11a88456acdeb
Mathieu Bourgon <54951088+MatBourgon@users.noreply.github.com>
2026-04-06 22:43:16 -0400

    RotMatrix (#56)
    * func_8003E940
    
    * file naming

---
+++ src/RotMatrix.c
@@ -0,0 +1,21 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", RotMatrix);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", RotMatrixX);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", RotMatrixY);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", RotMatrixZ);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", func_8003E2FC);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", func_8003E378);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", RotMatrixZYX);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", func_8003E65C);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", func_8003E758);
+
+INCLUDE_ASM("asm/nonmatchings/RotMatrix", func_8003E940);
