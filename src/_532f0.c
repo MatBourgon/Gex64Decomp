a714c92ba3192c2d823a2226a43a143bf74cca60
Retrofuge <tkmaxer19@gmail.com>
2026-07-03 18:28:14 +0000

    Update _532f0.c
    Added functions for func_80052A7C, func_800526F0 &  func_80052AA8

--- src/_532f0.c
+++ src/_532f0.c
@@ -1,6 +1,26 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/_532f0", func_800526F0);
+extern void func_80052954();                                  /* extern */
+extern int D_800E5CE0;
+extern char D_80154830;
+extern char D_80154831;
+extern char D_80154833;
+extern int D_80154838;
+extern int D_8015483C;
+extern int D_80154840;
+extern int D_80156854;
+
+void func_800526F0(void) {
+    D_80154838 = -1;
+    D_8015483C = -1;
+    D_80154833 = 1;
+    D_80154840 = 0;
+    D_80154831 = 0;
+    D_80154830 = 0;
+    D_800E5CE0 = 0;
+    D_80156854 = 0x4B0;
+    func_80052954();
+}
 
 INCLUDE_ASM("asm/nonmatchings/_532f0", func_80052758);
 
@@ -13,7 +33,6 @@ void func_80052944(void) {
 
 void func_8005294C(void) {
 }
-
 
 extern char D_80154832;
 extern int D_80154834;
@@ -37,11 +56,28 @@ void func_80052954(void) {
     D_80154838 = -1;
 }
 
+
 INCLUDE_ASM("asm/nonmatchings/_532f0", func_800529A8);
 
-INCLUDE_ASM("asm/nonmatchings/_532f0", func_80052A7C);
+extern int func_80052D18(int arg0,int arg1);                                 /* extern */
+extern int D_80154834;
 
-INCLUDE_ASM("asm/nonmatchings/_532f0", func_80052AA8);
+void func_80052A7C(void) {
+    int arg0;
+    if (D_80154834 == 0) {
+        func_80052D18(arg0, 7);
+    }
+}
+
+extern func_80052ADC(int);                                     /* extern */
+extern int func_80052FB8(void);                                /* extern */
+
+void func_80052AA8(int arg0) {
+    if (func_80052FB8() != 0) {
+        func_80052ADC(arg0);
+    }
+}
+
 
 INCLUDE_ASM("asm/nonmatchings/_532f0", func_80052ADC);
 
