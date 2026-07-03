c28ab4a721b56855738eb4002ad5d694c3f413bf
Mathieu Bourgon <matbourgon@gmail.com>
2026-07-03 18:32:22 +0000

    __rmonPanic and __remonSetComm

--- src/rmon/rmonmisc.c
+++ src/rmon/rmonmisc.c
@@ -1,9 +1,30 @@
 #include "common.h"
 
+#include "PRinternal/dbgproto.h"
+#include "PR/os_internal.h"
+#include "PR/sptask.h"
+#include "PRinternal/rmonint.h"
+
+#include "PRinternal/macros.h"
+
 INCLUDE_ASM("asm/nonmatchings/rmon/rmonmisc", __rmonSetFault);
 
 INCLUDE_ASM("asm/nonmatchings/rmon/rmonmisc", __rmonInit);
 
-INCLUDE_ASM("asm/nonmatchings/rmon/rmonmisc", func_80066D9C);
+void __rmonPanic()
+{
+    for(;;)
+    {
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/rmon/rmonmisc", __rmonSetComm);
+int __rmonSetComm(KKHeader* req) {
+    KKObjectEvent reply;
+
+    reply.header.code = req->code;
+    reply.object = 0;
+    reply.header.error = TV_ERROR_NO_ERROR;
+    
+    __rmonSendReply(&reply.header, sizeof(reply), KK_TYPE_REPLY);
+    return TV_ERROR_NO_ERROR;
+}
