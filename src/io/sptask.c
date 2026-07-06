c501facc0bf188a284a8a1d76ac7547440b3d121
Mathieu Bourgon <matbourgon@gmail.com>
2026-07-06 20:40:26 +0000

    sptask bss

--- src/io/sptask.c
+++ src/io/sptask.c
@@ -13,7 +13,7 @@
         ptr = (void*)osVirtualToPhysical(ptr); \
     } (void)0
 
-BSS_STATIC OSTask tmp_task;
+static OSTask tmp_task;
 
 static OSTask* _VirtualToPhysicalTask(OSTask* intp) {
     OSTask* tp;
