127b83424d58276d8e73a9f859da98a434c93b84
Mathieu Bourgon <matbourgon@gmail.com>
2026-07-06 16:30:34 +0000

    readhost.c

--- src/host/readhost.c
+++ src/host/readhost.c
@@ -1,3 +1,36 @@
-#include "common.h"
+#ifndef _FINALROM
 
-INCLUDE_ASM("asm/nonmatchings/host/readhost", osReadHost);
+#include "PR/os_internal.h"
+#include "PR/rdb.h"
+
+#include "PRinternal/macros.h"
+
+static int readHostInitialized = FALSE;
+BSS_STATIC OSMesgQueue readHostMesgQueue ALIGNED(0x8);
+BSS_STATIC OSMesg readHostMesgBuf[1];
+
+u32 __osRdb_Read_Data_Buf;
+u32 __osRdb_Read_Data_Ct;
+
+void osReadHost(void* dramAddr, u32 nbytes) {
+    char tstr[4];
+    u32 sent = 0;
+
+    if (!readHostInitialized) {
+        osCreateMesgQueue(&readHostMesgQueue, readHostMesgBuf, ARRLEN(readHostMesgBuf));
+        osSetEventMesg(OS_EVENT_RDB_READ_DONE, &readHostMesgQueue, NULL);
+        readHostInitialized = TRUE;
+    }
+
+    __osRdb_Read_Data_Buf = dramAddr;
+    __osRdb_Read_Data_Ct = nbytes;
+
+    while (sent == 0) {
+        sent += __osRdbSend(tstr, 1, RDB_TYPE_GtoH_READY_FOR_DATA);
+    }
+
+    osRecvMesg(&readHostMesgQueue, NULL, OS_MESG_BLOCK);
+    return;
+}
+
+#endif
