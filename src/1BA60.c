35a4c973480a155438e20eca415b2640cc437c94
Retrofuge <tkmaxer19@gmail.com>
2026-07-10 00:27:46 +0000

    Update 1BA60.c
    Added more functions.

--- src/1BA60.c
+++ src/1BA60.c
@@ -5,7 +5,18 @@ INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001AE60);
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001B760);
 
-INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001B7C0);
+int func_8001B7C0(int* arg0) {
+    int temp_v0;
+    int temp_a0;
+    temp_v0 = *(int*)((int)arg0 + 0xD8) + *(int*)((int)arg0 + 0xE4);
+    temp_a0 = -*(int*)((int)arg0 + 0xF0);
+    *(int*)((int)arg0 + 0xD8) = temp_v0;
+
+    if (temp_v0 < temp_a0) {
+        *(int*)((int)arg0 + 0xD8) = temp_a0;
+    }
+    *(unsigned short*)((int)arg0 + 0x4C) += *(int*)((int)arg0 + 0xD8);
+}
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001B804);
 
@@ -43,7 +54,19 @@ INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001C978);
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001CB9C);
 
-INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001CDFC);
+void func_8001CDFC(short* arg0, short arg1, short arg2) {
+    short temp_v0;
+    short temp_v0_2;
+
+    temp_v0 = *arg0;
+    if ((temp_v0 + arg2) < arg1) {
+        *arg0 = temp_v0 + arg2;
+    }
+    temp_v0_2 = *arg0;
+    if (arg1 < (temp_v0_2 - arg2)) {
+        *arg0 = temp_v0_2 - arg2;
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001CE54);
 
@@ -76,7 +99,21 @@ INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001DA8C);
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001DC80);
 
-INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001DDD8);
+int func_8001DDD8(int* arg0, int* arg1, int* arg2) {
+    if (!(*(unsigned short*)((int)arg2 + 0x8A) & 1)) {
+        *(int*)((int)arg0 + 0xF8) = 0x40;
+        *(unsigned char*)((int)arg0 + 0x4E) = 0x13;
+        *(unsigned short*)((int)arg0 + 0x5E) = 0;
+        
+        *(unsigned short*)((int)arg2 + 0x148) = *(unsigned short*)((int)arg0 + 0x4C);
+        
+        if (*(unsigned short*)((int)arg2 + 0x8A) & 0x100) {
+            *(unsigned short*)((int)arg2 + 0xEC) = 0;
+        } else {
+            *(unsigned short*)((int)arg2 + 0xEC) = *(unsigned short*)((int)arg2 + 0x10);
+        }
+    }
+}
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001DE2C);
 
@@ -86,9 +123,23 @@ INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001DFC0);
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001E064);
 
-INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001E0C4);
+int func_8001E0C4(int* arg0, int* arg1, int* arg2, int* arg3) {
+    if ((*(int*)((int)arg3 + 4) & 0x10) && !(*(int*)((int)arg0 + 0xFC) & 0x8000)) {
+        *(int*)((int)arg0 + 0xF8) = 0x20;
+        *(unsigned char*)((int)arg0 + 0x4E) = 0x19;
+        *(unsigned short*)((int)arg0 + 0x5E) = 0;
+        *(int*)((int)arg0 + 0xFC) &= ~8;
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001E114);
+int func_8001E114(int* arg0, int* arg1, int* arg2, int* arg3) {
+    if ((*(int*)((int)arg3 + 4) & 0x3000) && !(*(int*)((int)arg0 + 0xFC) & 0x8000)) {
+        *(int*)((int)arg0 + 0xF8) = 0x1000;
+        *(unsigned char*)((int)arg0 + 0x4E) = 0x1B;
+        *(unsigned short*)((int)arg0 + 0x5E) = 0;
+    }
+}
+
 
 INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001E154);
 
@@ -120,4 +171,10 @@ void func_8001E980(int* arg0, int arg1, unsigned short* arg2) {
     }
 }
 
-INCLUDE_ASM("asm/nonmatchings/1BA60", func_8001E9AC);
+int func_8001E9AC(int* arg0, int* arg1, int* arg2) {
+    if ((*(unsigned short*)((int)arg2 + 0x8A) & 0x40) && !(*(int*)((int)arg0 + 0xFC) & 0x8000)) {
+        *(int*)((int)arg0 + 0xF8) = 0x800;
+        *(unsigned char*)((int)arg0 + 0x4E) = 0x17;
+        *(unsigned short*)((int)arg0 + 0x5E) = 0;
+    }
+}
